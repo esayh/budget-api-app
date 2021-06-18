93ab369aeebcfd18c9c3a408639ff91bbdb73aff
EsayH <ez@EsayHs-Mac-mini.local>
2021-06-18 18:24:53 -0400

    add port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = require('./app')
 
 //Configuration
 require('dotenv').config()
-const PORT = process.env.PORT || 3003
+const PORT = process.env.PORT || 9090
 
 //Listen
 app.listen(PORT, () => console.log(`Server Running on port: http://localhost:${PORT}`))
